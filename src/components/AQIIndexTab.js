1c46619b368f97340586b52ed40cd08bbab909b1
Amrit Singh <amrit.sbagga@gmail.com>
2021-11-23 02:01:12 +0530

    Added console log to test ws url

--- src/components/AQIIndexTab.js
+++ src/components/AQIIndexTab.js
@@ -9,6 +9,7 @@ import { foundAQIColor } from '../utils/Utility';
 const AQIIndexTab = () => {
 
     const WS_CITY_AQI_URL = process.env.WS_CITY_AQI_API_URL;
+    console.log("WS_CITY_AQI_URL = ", WS_CITY_AQI_URL);
     const cityAQIArray = useMemo(() => [], []);
     const MAX_AQI_POINTS_PER_GRAPH = 5;
 
