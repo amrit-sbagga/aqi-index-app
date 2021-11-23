9ec85cdb6e797b7e7c5a009203709eebfe5fd22b
Amrit Singh <amrit.sbagga@gmail.com>
2021-11-23 17:35:35 +0530

    fixed UI issues

--- src/components/AQICategory.js
+++ src/components/AQICategory.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const AQICategory = () => {
     return (
-        <div style={{width: '100%', display:'flex', height:'80px', textAlign:'center'}}>
-        <div style={{width: '16%', float: 'left', background: '#55a84f'}}>  <small>Good<br/>(0-50) </small></div>
-        <div style={{background: '#a3c853', width: '16%'}}> <small>Satisfactory <br/>(51-100) </small></div>
-        <div style={{background: '#fff833', width: '16%'}}> <small>Moderate <br/>(101-200) </small></div>
-        <div style={{background: '#f29c33', width: '16%'}}> <small>Poor <br/>(201-300)</small> </div>
-        <div style={{background: '#e93f33', width: '20%'}}> <small>Very Poor<br/>(301-400) </small></div>
-        <div style={{background: '#af2d24', width: '16%'}}> <small>Severe <br/>(400-500) </small> </div>
-      </div>
+        <div style={{width: '100%', display:'flex', height:'20%', textAlign:'center', overflow:'hidden'}}>
+          <div style={{width: '16%', float: 'left', background: '#55a84f'}}>  <span><small>Good<br/>(0-50) </small></span></div>
+          <div style={{background: '#a3c853', width: '16%'}}> <span><small>Satisfactory <br/>(51-100) </small></span></div>
+          <div style={{background: '#fff833', width: '16%'}}> <span><small>Moderate <br/>(101-200) </small></span></div>
+          <div style={{background: '#f29c33', width: '16%'}}> <span><small>Poor <br/>(201-300)</small></span></div>
+          <div style={{background: '#e93f33', width: '20%'}}> <span><small>Very Poor<br/>(301-400) </small></span></div>
+          <div style={{background: '#af2d24', width: '16%'}}> <span><small>Severe <br/>(400-500) </small></span></div>
+        </div>
     )
 }
 
--- src/components/AQIIndexTab.js
+++ src/components/AQIIndexTab.js
@@ -2,16 +2,16 @@ import React, { useState, useCallback, useEffect, useMemo, useRef } from 'react'
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import ShowGraph from './ShowGraph';
 import moment from 'moment';
-import './AQIIndexTab.css';
+import '../assets/css/AQIIndexTab.css';
 import AQICategory from './AQICategory';
 import { foundAQIColor } from '../utils/Utility'; 
 
 const AQIIndexTab = () => {
 
-    const WS_CITY_AQI_URL = "wss://city-ws.herokuapp.com"; //process.env.WS_CITY_AQI_API_URL;
+    const WS_CITY_AQI_URL = process.env.WS_CITY_AQI_API_URL;
     //console.log("WS_CITY_AQI_URL = ", WS_CITY_AQI_URL);
     const cityAQIArray = useMemo(() => [], []);
-    const MAX_AQI_POINTS_PER_GRAPH = 5;
+    const MAX_AQI_POINTS_PER_GRAPH = 4;
 
     const ref = useRef(null);
     const refClose = useRef(null);
@@ -19,6 +19,7 @@ const AQIIndexTab = () => {
     const [showGraphStatus, setShowGraphStatus] = useState(false);
     const [socketUrl, setSocketUrl] = useState(WS_CITY_AQI_URL);
     const [items, setItems] = useState([]);
+    const [wsStatusText, setWsStatusText] = useState('Close Connection');
 
     const {
         lastMessage,
@@ -33,25 +34,19 @@ const AQIIndexTab = () => {
                 foundObj.prevaqi = parseFloat(foundObj?.aqi).toFixed(2);
                 foundObj.aqi = parseFloat(dataObject?.aqi).toFixed(2);
                 foundObj.lastUpdatedAt = moment().format('DD/MM/YYYY hh:mm:ss');
-                //let city = foundObj.city;
                 //save last 5 aqi value for graph
                 if(foundObj.aqiValList.length > MAX_AQI_POINTS_PER_GRAPH){
                     foundObj.aqiValList.pop();
                 } else{
-                foundObj.aqiValList.unshift(foundObj.aqi);
+                    foundObj.aqiValList.unshift(foundObj.aqi);
                 }
 
                 //save last 5 aqi value update time history for graph
                 if(foundObj.aqiValTimeList.length > MAX_AQI_POINTS_PER_GRAPH){
-                foundObj.aqiValTimeList.pop();
+                    foundObj.aqiValTimeList.pop();
                 } else{
-                foundObj.aqiValTimeList.unshift(foundObj.lastUpdatedAt);
+                    foundObj.aqiValTimeList.unshift(foundObj.lastUpdatedAt);
                 }
-
-                //  if(city === "Indore"){
-                //     console.log("foundObj for ", city, "--->", foundObj);
-                //  }
-                
             } else{
                 //console.log("push val here..!!");
                 dataObject.aqi = parseFloat(dataObject?.aqi).toFixed(2);
@@ -71,14 +66,6 @@ const AQIIndexTab = () => {
         }
     } , [lastMessage, pushToAQIArray, cityAQIArray]);
 
-    const handleClickChangeSocketUrl = useCallback(() =>
-        setSocketUrl(WS_CITY_AQI_URL), []);
-
-    const handleSocketClose = () => {
-        console.log("close socket connection..!!");
-        setSocketUrl(null);
-    }
-
     const handleRowClick = (listValue) => {
         setShowGraphStatus(true);
         setSelectedRow(listValue);
@@ -93,67 +80,86 @@ const AQIIndexTab = () => {
         [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
     }[readyState];
 
+    const handleClickSocketChangeStatus = useCallback((connStatus) => {
+        //console.log("connStatus here -> ", connStatus);
+        if(connStatus === "Open"){
+            setSocketUrl(null);
+            setWsStatusText("Open Connection")
+        } else {
+            setSocketUrl(WS_CITY_AQI_URL);
+            setWsStatusText("Close Connection")
+        } 
+    }, []);
+        
   return (
-    <div style={{fontSize:'1.5rem', textAlign:'center'}}>
-        <button onClick={handleSocketClose}>
-        Click Me to close/unsubscribe Socket Url
-      </button>
-        <button style={{marginLeft:'25px'}} onClick={handleClickChangeSocketUrl}>
-            Click to subscribe again
-        </button>
-        <br/>
-        <span>The WebSocket is currently {connectionStatus}</span>
-        <br/>
-        <AQICategory />
-        <br/>
-        <button type="button" ref={ref} className="btn btn-primary d-none" data-toggle="modal" data-target="#showGraphModel">
-        Launch modal
-        </button>
-        <div className="modal fade" id="showGraphModel" tabIndex="-1" role="dialog" aria-labelledby="showGraphModelTitle" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="graphModalLongTitle">AQI Graph</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p> Displaying Last 5 AQI for {selectedRow.city}</p>
-                            { showGraphStatus && <ShowGraph selectedRowData={selectedRow}/>}
-                         </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" 
-                            data-dismiss="modal" ref={refClose}
-                            onClick={() => { setShowGraphStatus(false); }}
-                            >Close</button>
+    <div>
+        <>
+            <div style={{width: '100%', display:'flex', height:'25%', textAlign:'center', marginTop: '5px'}}>
+                <div style={{width: '80%'}}> 
+                    <span style={{float: 'right'}}><small>The WebSocket is currently {connectionStatus}</small></span>
+                </div>
+                <div style={{width: '20%', float: 'right',
+                             marginRight:'5px', marginLeft:'5px'
+                             }}>
+                    <button style={{ background:'white', color:'black',
+                             border: '2px solid #008CBA', borderRadius:'8px'}}
+                             onClick={() => handleClickSocketChangeStatus(connectionStatus)}>
+                        <span style={{float: 'right'}}><small>{ wsStatusText }</small></span>
+                    </button>
+                </div>
+            </div>
+        </>
+        <div style={{fontSize:'1.5rem', textAlign:'center', marginTop : '5px'}}>
+            <AQICategory />
+            <br/>
+            <button type="button" ref={ref} className="btn btn-primary d-none" data-toggle="modal" data-target="#showGraphModel">
+            </button>
+            <div className="modal fade" id="showGraphModel" tabIndex="-1" role="dialog" aria-labelledby="showGraphModelTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="graphModalLongTitle">AQI Graph</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <p> Displaying Last {selectedRow?.aqiValList?.length} AQI for {selectedRow.city}</p>
+                                { showGraphStatus && <ShowGraph selectedRowData={selectedRow}/>}
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" 
+                                data-dismiss="modal" ref={refClose}
+                                onClick={() => { setShowGraphStatus(false); }}
+                                >Close</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <table className="table table-bordered table-hover">
-            <thead>
-                <tr className="font-link center-bold table-primary">
-                <th scope="col">City</th>
-                <th scope="col">Current AQI</th>
-                <th scope="col">Last Updated</th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map(( listValue, index ) => {
-                        return (
-                            <tr key={index} className="font-link" 
-                                    onClick={() => { 
-                                        handleRowClick(listValue)
-                                }}>
-                                <td>{listValue?.city}</td>
-                                <td style={{color: foundAQIColor(listValue.aqi)}}>{listValue?.aqi}</td>
-                                <td>{listValue?.lastUpdatedAt}</td>
-                            </tr>
-                        );
-                        })}
-            </tbody>
+            <table className="table table-bordered table-hover">
+                <thead>
+                    <tr className="font-link center-bold table-primary">
+                    <th scope="col">City</th>
+                    <th scope="col">Current AQI</th>
+                    <th scope="col">Last Updated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map(( listValue, index ) => {
+                            return (
+                                <tr key={index} className="font-link" 
+                                        onClick={() => { 
+                                            handleRowClick(listValue)
+                                    }}>
+                                    <td>{listValue?.city}</td>
+                                    <td style={{color: foundAQIColor(listValue.aqi)}}>{listValue?.aqi}</td>
+                                    <td>{listValue?.lastUpdatedAt}</td>
+                                </tr>
+                            );
+                            })}
+                </tbody>
         </table>
+        </div>
     </div>
     );
 };
